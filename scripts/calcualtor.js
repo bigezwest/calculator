07c157025d6a3ec96f80f6204eb5ab8ad86a6b13
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-10 08:15:46 +0200

    feature(operate()): Add function
    * This will take 3 arguments, num1, num2, and operator.  The operator
    is used to select the case in the switch statement, and then num1, and
    num2 are used as arguments to call the appropriate function.
    * Default will be set up later, depending on the behavior of the
    calculator.

--- scripts/calcualtor.js
+++ scripts/calcualtor.js
@@ -5,6 +5,24 @@ let num1 = null;
 let num2 = null;
 let operator = null;
 
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case ("+"):
+            add(num1, num2);
+            break;
+        case ("-"):
+            subtract(num1, num2);
+            break;
+        case ("*"):
+            multiply(num1, num2);
+            break;
+        case ("/"):
+            divide(num1, num2);
+            break;
+        default:
+            /* Error and then clear or back up */
+    }
+}
 
 function add(a, b) {
     return a + b;
