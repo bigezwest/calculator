5a21d183094495e2373a237ee69d66bf0b5cbfdf
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-10 08:02:35 +0200

    feature(divide(a, b)): Add function, test

--- scripts/calcualtor.js
+++ scripts/calcualtor.js
@@ -12,6 +12,11 @@ function multiply(a, b) {
     return a * b;
 }
 
+function divide(a, b) {
+    return a / b;
+}
+
 console.log("1 + 2 = " + add(1, 2));
 console.log("2 - 1 = " + subtract(2, 1));
 console.log("1 * 2 = " + multiply(1, 2));
+console.log("2 / 1 = " + divide(2, 1));
