bee393f547a66797182a24238a155c5cc9d69c42
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-10 07:51:50 +0200

    feature(multiply(a, b)): Add function, test

--- scripts/calcualtor.js
+++ scripts/calcualtor.js
@@ -8,5 +8,10 @@ function subtract(a, b) {
     return a - b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
 console.log("1 + 2 = " + add(1, 2));
 console.log("2 - 1 = " + subtract(2, 1));
+console.log("1 * 2 = " + multiply(1, 2));
