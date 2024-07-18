b4bc6828af2d15929230c02a8504d594f2e5a6ca
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-18 05:33:24 +0200

    chore(remove comments)

--- scripts/calcualtor.js
+++ scripts/calcualtor.js
@@ -54,8 +54,3 @@ buttons.forEach((button) => {
         textResult.innerHTML = tempNum;
     })
 });
-
-console.log("1 + 2 = " + add(1, 2));
-console.log("2 - 1 = " + subtract(2, 1));
-console.log("1 * 2 = " + multiply(1, 2));
-console.log("2 / 1 = " + divide(2, 1));
