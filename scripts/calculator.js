2480fa29407adfdcaac3649d457778c968f6ae86
Thomas <88990664+bigezwest@users.noreply.github.com>
2024-07-25 17:59:49 +0200

    feature(add): Initial set up
    * This allows the numbers, and operator to be shown as they are typed.
    * It also performs add() successfully.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,14 +1,20 @@
 "use strict";
 
 // Variables to hold operations - use to display on calculator -----------------
+const calcDisplay = document.querySelector('textarea');
+const numButtons = document.querySelectorAll('.num_button');
+const opButtons = document.querySelectorAll('.op_button');
+const equalsButton = document.querySelector('.equals_button');
+
 let num1 = null;
 let num2 = null;
 let operator = null;
+let result = null;
 
 function operate(num1, num2, operator) {
     switch (operator) {
         case ("+"):
-            add(num1, num2);
+            return add(num1, num2);
             break;
         case ("-"):
             subtract(num1, num2);
@@ -25,7 +31,7 @@ function operate(num1, num2, operator) {
 }
 
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -40,11 +46,43 @@ function divide(a, b) {
     return a / b;
 }
 
-const calcDisplay = document.querySelector('textarea');
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
+let isNum1Set = false;
+let isNum2Set = false;
+let isOperatorSet = false;
+
+numButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
         let tempNum = event.target.innerHTML;
-        calcDisplay.innerHTML = tempNum;
+        if (!isNum1Set) {
+            if (num1 === null) {
+                num1 = tempNum;
+            } else if (!isNum1Set) {
+                num1 += tempNum;
+            }
+            calcDisplay.innerHTML = num1;
+        } else if (isNum1Set) {
+            if (num2 === null) {
+                num2 = tempNum;
+            } else if (!isNum2Set) {
+                num2 += tempNum;
+            }
+            calcDisplay.innerHTML += num2;
+        }
     })
+});
+
+opButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        if (isOperatorSet === false) {
+            operator = event.target.innerHTML;
+            isNum1Set = true;
+        }
+        calcDisplay.innerHTML += operator;
+        isOperatorSet = true;
+    })
+});
+
+equalsButton.addEventListener("click", function() {
+    let result = operate(num1, num2, operator);
+    calcDisplay.innerHTML = result;
 });
